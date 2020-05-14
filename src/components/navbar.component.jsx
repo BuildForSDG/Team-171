5e30b636910ec529f277b8137654ec26d71f15f0
antony_kariu <kariuantony@gmail.com>
2020-05-14 08:40:27 +0300

    feat(Front end Routing): Enables navigation of the application
    links components with the Link method from react-router-dom and displays the links with Route method from react-router-dom

---
+++ src/components/navbar.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/" className="nav-link">Home</Link></li>
+        <li><Link to="/about" className="nav-link">About</Link></li>
+        <li><Link to="/contact" className="nav-link">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
