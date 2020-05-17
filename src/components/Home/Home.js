bee95bbe67ead9adb22ba9ca14664ecba8cbb76e
antony_kariu <kariuantony@gmail.com>
2020-05-18 01:38:10 +0300

    feat(Firebase-Auth, Redux-store,): Enables authentication via firebase and sets up redux store
    Adds authentication functionality to the rect-app and setsup state management with redux and react-redux, Handles front-end routing

---
+++ src/components/Home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions";
+
+const Home = (props) => {
+  const {isLoggingOut, logoutError} = props; 
+  const handleLogout = () => {
+    const {dispatch}  = props;
+    dispatch(logoutUser());
+  }
+  return(
+    <div>
+      <h1>You have reached the homepage</h1>
+      <p>Any routes here will also be protected</p>
+      <button onClick={handleLogout}>Logout</button>
+      {isLoggingOut && <p>Logging Out....</p>}
+      {logoutError && <p>Error logging out</p>}
+    </div>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    isLoggingOut: state.auth.isLoggingOut,
+    logoutError: state.auth.logoutError
+  };
+}
+
+export default connect (mapStateToProps)(Home);
