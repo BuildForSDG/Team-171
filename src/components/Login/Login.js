3e109cef9b600a6fd50b8021917782d4e2253c84
antony_kariu <kariuantony@gmail.com>
2020-05-18 21:27:56 +0300

    feat(sign up): implemented sign up
    enable creating of a user and authentication with the created user

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,49 +1,56 @@
 import React from "react";
 
 import { connect } from "react-redux";
-import { Redirect } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
 
 import { useFormik } from 'formik';
 import { loginUser } from "../../actions";
 
 const Login = (props) => {
-  const {isAuthenticated, loginError} = props;
+  const {isAuthenticated, loginError, dispatch} = props;
+
   const formik = useFormik({
     initialValues: {
       email: '',
       password: '',
     },
     onSubmit: values => {
-      const {dispatch} = props;
       const {email, password} = values;
       dispatch(loginUser(email, password));
-      // alert(JSON.stringify(values, null, 2));
     },
-  });
+  }); 
+
   if (isAuthenticated) {
     return <Redirect to="/" />
   } else {
     return (
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          onChange={formik.handleChange}
-          value={formik.values.email}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          name="password"
-          type="password"
-          onChange={formik.handleChange}
-          value={formik.values.password}
-        />
-        {loginError && (<p>Incorrect email or password</p>)}
-        <button type="submit">Submit</button>
-      </form>
+      <div>
+      <div>
+        <Link to="/signup">Create Account</Link>
+      </div>
+      <div>
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
+          />
+          {loginError && (<p>Incorrect email or password</p>)}
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      </div>
     );
   }
 };
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,17 +2,30 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "team-171.firebaseapp.com",
-  databaseURL: "https://team-171.firebaseio.com",
-  projectId: "team-171",
-  storageBucket: "team-171.appspot.com",
-  messagingSenderId: "156600228620",
-  appId: "1:156600228620:web:1ee875eddfc4e1ac924729",
-  measurementId: "G-900LM3QSZQ",
+const devConfig = {
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_DEV_APP_ID,
+  measurementId: process.env.REACT_APP_DEV_MEASUREMENT_ID,
 }
 
-export const myFirebase = firebase.initializeApp(firebaseConfig);
+const prodConfig = {
+  apiKey: process.env.REACT_APP_PROD_API_KEY,
+  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_PROD_APP_ID,
+  measurementId: process.env.REACT_APP_PROD_MEASUREMENT_ID,
+}
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+export const myFirebase = firebase.initializeApp(config);
 const baseDb = myFirebase.firestore();
 export const db = baseDb;
