4ee6c2e9077b04a941fcfd3aab135f31f9433cc9
antony_kariu <kariuantony@gmail.com>
2020-05-20 10:36:09 +0300

    build(eslint): Adds linting to application

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,80 +3,80 @@ import React from "react";
 import { connect } from "react-redux";
 import { Redirect, Link } from "react-router-dom";
 
-import { useFormik } from 'formik';
+import { useFormik } from "formik";
 import { loginUser } from "../../actions";
 import "../../styles/login.scss";
 
 const Login = (props) => {
-  const {isAuthenticated, loginError, dispatch} = props;
+  const { isAuthenticated, loginError, dispatch } = props;
 
   const formik = useFormik({
     initialValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: ""
     },
     onSubmit: values => {
-      const {email, password} = values;
+      const { email, password } = values;
       dispatch(loginUser(email, password));
-    },
-  }); 
+    }
+  });
 
   if (isAuthenticated) {
-    return <Redirect to="/" />
+    return <Redirect to="/" />;
   } else {
     return (
       <div className="login">
-      <div className="cta-section">
-        <div className="cta-head">
-          <p>Building Your <span>Brand</span></p>
-          <p>Made Easy</p>
-        </div>
-        <div className="cta-des">
+        <div className="cta-section">
+          <div className="cta-head">
+            <p>Building Your <span>Brand</span></p>
+            <p>Made Easy</p>
+          </div>
+          <div className="cta-des">
           <p>loremAdipisicing deserunt ex consectetur dolor aute excepteur qui et.</p>
         </div>
-      </div>
-      <div className="form-view">
-        <div className="signup-link">
-          <p>Don't have an account? <Link to="/signup" className="link" style={{textDecoration: 'none'}}><span>Sign up</span></Link></p>
         </div>
-      <div className="form">
-        <div className="form-title">
-          <p>Login</p>
+        <div className="form-view">
+          <div className="signup-link">
+            <p>Don't have an account? <Link to="/signup" className="link" style={{ textDecoration: "none" }}><span>Sign up</span></Link></p>
+          </div>
+          <div className="form">
+            <div className="form-title">
+              <p>Login</p>
+            </div>
+            <form onSubmit={formik.handleSubmit}>
+              <label htmlFor="email">E-mail</label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                onChange={formik.handleChange}
+                value={formik.values.email}
+                placeholder="example@email.com"
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                onChange={formik.handleChange}
+                value={formik.values.password}
+                placeholder="password"
+              />
+              {loginError && (<p>Incorrect email or password</p>)}
+              <button type="submit" className="login-btn">LOGIN</button>
+            </form>
+          </div>
         </div>
-        <form onSubmit={formik.handleSubmit}>
-          <label htmlFor="email">E-mail</label>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            placeholder="example@email.com"
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            value={formik.values.password}
-            placeholder="password"
-          />
-          {loginError && (<p>Incorrect email or password</p>)}
-          <button type="submit" className="login-btn">LOGIN</button>
-        </form>
-        </div>
-      </div>
       </div>
     );
   }
 };
 
-function mapStateToprops(state) {
+function mapStateToprops (state) {
   return {
     isLoggingIn: state.auth.isLoggingIn,
     loginError: state.auth.loginError,
-    isAuthenticated: state.auth.isAuthenticated,
+    isAuthenticated: state.auth.isAuthenticated
   };
 }
 
